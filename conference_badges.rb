c91aee167fe9718e314c5fc36073c2b9126473b3
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-23 17:56:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,11 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(nameArr)
+  return nameArr.collect do |e|
+    badge_maker(e)
+  end
+end
+
+def assign_rooms(nameArr)
